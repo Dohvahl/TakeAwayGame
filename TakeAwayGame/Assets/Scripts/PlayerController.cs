319e5b7c4ff51022d67536a7c00914c0bff9e47f
Tyler <ty.r.dave@gmail.com>
2017-08-29 17:28:03 -0600

    Added horizontal movement
    Added arrows/WASD horizontal movement

---
+++ TakeAwayGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkingSpeed;
+
+	private CharacterController character;
+
+	void Start()
+	{
+		character = GetComponent<CharacterController> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorz = Input.GetAxis ("Horizontal");
+		Vector3 movement = new Vector3 (moveHorz, 0.0f, 0.0f);
+		Move(transform.TransformDirection (movement), walkingSpeed);
+	}
+
+	void Move (Vector3 movement, float speed)
+	{
+		character.Move (movement * speed * Time.deltaTime);
+	}
+}
