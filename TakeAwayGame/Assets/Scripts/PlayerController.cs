5513c39cafd57289a7feef4cebcccbaf712ba026
Tyler <ty.r.dave@gmail.com>
2017-08-30 16:21:46 -0600

    Implemented running
    Hold Left Shift to run

--- TakeAwayGame/Assets/Scripts/PlayerController.cs
+++ TakeAwayGame/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float walkingSpeed;
+	public float baseSpeed;
 	public float jumpHeight;
 
 	private Rigidbody2D character;
 
+	private float walkingSpeed;
+	private float maxRunSpeed = 30;
+	private float runningAccel = 0.5f;
+
 	void Start()
 	{
 		character = GetComponent<Rigidbody2D> ();
+		walkingSpeed = baseSpeed;
 	}
 
 	void FixedUpdate()
 	{
 		float moveHorz = Input.GetAxis ("Horizontal");
+
+		if (Input.GetKey (KeyCode.LeftShift) && walkingSpeed < maxRunSpeed)
+			walkingSpeed += runningAccel;
+		else if (!Input.GetKey(KeyCode.LeftShift) && walkingSpeed > baseSpeed)
+			walkingSpeed -= runningAccel;
+		
 		Vector2 movement = new Vector2 (moveHorz * walkingSpeed, character.velocity.y);
 		if (Input.GetButtonDown ("Jump")) {
 			movement.y = jumpHeight;
