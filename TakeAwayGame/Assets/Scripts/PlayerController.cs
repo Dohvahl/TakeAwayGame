e154dc838f253516b56118d88fb43ab193422ecc
Tyler <ty.r.dave@gmail.com>
2017-08-30 16:01:31 -0600

    fixed movement and gravity issues
    Decided to change velocity directly as it seems a much simpler solution.

--- TakeAwayGame/Assets/Scripts/PlayerController.cs
+++ TakeAwayGame/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float walkingSpeed;
+	public float jumpHeight;
 
-	private CharacterController character;
+	private Rigidbody2D character;
 
 	void Start()
 	{
-		character = GetComponent<CharacterController> ();
+		character = GetComponent<Rigidbody2D> ();
 	}
 
 	void FixedUpdate()
 	{
 		float moveHorz = Input.GetAxis ("Horizontal");
-		Vector3 movement = new Vector3 (moveHorz, 0.0f, 0.0f);
-		Move(transform.TransformDirection (movement), walkingSpeed);
-	}
-
-	void Move (Vector3 movement, float speed)
-	{
-		character.Move (movement * speed * Time.deltaTime);
+		Vector2 movement = new Vector2 (moveHorz * walkingSpeed, character.velocity.y);
+		if (Input.GetButtonDown ("Jump")) {
+			movement.y = jumpHeight;
+		}
+		character.velocity = movement;
 	}
 }
